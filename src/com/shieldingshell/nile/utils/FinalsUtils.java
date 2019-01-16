a2240de46fd2cb913b40a13648fe38d9db8f86df
mwarko <mafournier@humanbooster.com>
2019-01-16 16:29:25 +0100

    finalsutils OK

--- src/com/shieldingshell/nile/utils/FinalsUtils.java
+++ src/com/shieldingshell/nile/utils/FinalsUtils.java
@@ -1,5 +1,7 @@
 package com.shieldingshell.nile.utils;
 
 public class FinalsUtils {
-
+	public static String DISK_NAME = "D:";
+	public static String FILE_SEPARATOR = "\\";
+	public static String TEST_FOLDER = DISK_NAME + FILE_SEPARATOR + "nileproject";
 }
