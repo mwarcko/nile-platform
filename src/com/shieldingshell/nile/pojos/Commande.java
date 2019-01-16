bfd16ce8e4a96a11f651c57ae2d328eaf432a257
mwarko <mafournier@humanbooster.com>
2019-01-16 16:21:52 +0100

    commande OK

--- src/com/shieldingshell/nile/pojos/Commande.java
+++ src/com/shieldingshell/nile/pojos/Commande.java
@@ -1,5 +1,25 @@
 package com.shieldingshell.nile.pojos;
 
-public class Commande {
+import java.util.List;
 
+public class Commande {
+	List<Carton> cartons;
+
+	public Commande(List<Carton> cartons) {
+		super();
+		this.cartons = cartons;
+	}
+
+	public final List<Carton> getCartons() {
+		return cartons;
+	}
+
+	public final void setCartons(List<Carton> cartons) {
+		this.cartons = cartons;
+	}
+
+	@Override
+	public String toString() {
+		return "Commande [cartons=" + cartons + "]";
+	}
 }
